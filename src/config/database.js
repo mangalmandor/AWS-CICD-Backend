d6b06818058a37ec8e8c00a767fe6996cc23be2d
Mangal Mandor <mangal.mandor13102003@therapidhire.com>
2026-04-06 16:04:50 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log(process.env.MONGO_URI);
     } catch (error) {
         process.exit(1);
     }
