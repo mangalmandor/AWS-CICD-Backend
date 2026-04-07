a461798607894c03e6c54052429bf290fd51e43c
Mangal Mandor <mangal.mandor13102003@therapidhire.com>
2026-04-07 18:02:50 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,16 +4,17 @@ const http = require('http');
 const app = require('./app');
 const { connectDB } = require('./config/database');
 const { initSocket } = require('./services/socketService');
+const initItemsCleanup = require('./utils/cronJobs');
 
 const server = http.createServer(app);
 
 initSocket(server);
-initItemsCleanup();
 
 const PORT = process.env.PORT || 5000;
 
 connectDB()
     .then(() => {
+initItemsCleanup();
         server.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
             console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
