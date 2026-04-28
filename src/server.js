ffea4f00a0f0021a683cbcb4770a4c14074b51e7
Mangal Mandor <mangal.mandor13102003@therapidhire.com>
2026-04-28 12:34:23 +0530

    Update server opened port for all request.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
     .then(() => {
 initItemsCleanup();
-        server.listen(PORT, () => {
+        server.listen(PORT,'0.0.0.0', () => {
             console.log(`Server running on port ${PORT}`);
             console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
         });
